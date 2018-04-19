0b3b54b8fdf931201bf523c73237c09259159ce1
José Emilio Gutiérrez Morán <36626945+gutierrezmoran@users.noreply.github.com>
2018-04-19 17:45:32 +0200

    + Comprobador implementado y testeado

--- src/control/Comprobador.java
+++ src/control/Comprobador.java
@@ -2,6 +2,11 @@ package control;
 
 import java.awt.Color;
 import java.util.AbstractCollection;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import org.hamcrest.core.IsInstanceOf;
 
 public class Comprobador {
 
@@ -18,24 +23,51 @@ public class Comprobador {
 		return comprobador;
 	}
 	
-	public boolean isColeccionLlena(Object coleccion, int limite) {
-		if (((AbstractCollection) coleccion).size() >= limite) {
+	/**
+	 * Comprueba si una coleccion esta llena
+	 * 
+	 * @param coleccion
+	 * @param limite
+	 * @return Retorna TRUE en caso de que este llena o FALSE en caso contrario.
+	 */
+	public boolean isColeccionLlena(AbstractCollection<Color> coleccion, int limite) {
+		if (coleccion.size() >= limite) {
 			return true;
 		}
 		
 		return false;
 	}
 	
-	public boolean comprobarColoresContiguos(Object coleccion) {
+	/**
+	 * Comprueba si dentro de una coleccion hay dos colores iguales y contiguos.
+	 * 
+	 * @param coleccion
+	 * @return Retorna TRUE si hay dos colores iguales y contiguos o FALSE en caso contrario.
+	 */
+	public boolean isColoresIgualesContiguos(AbstractCollection<Color> coleccion) {
+		for (int i = 0; i < coleccion.size() - 1; i++) {
+			if(comprobarColoresIguales(getColor(coleccion, i), getColor(coleccion, i + 1))) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 	
-	public boolean comprobarColoresIguales(Color color1, Color color2) {
+	private boolean comprobarColoresIguales(Color color1, Color color2) {
 		if (color1 == color2) {
 			return true;
 		}
 		
 		return false;
+	}
+	
+	private Color getColor(AbstractCollection<Color> coleccion, int posicion) {
+		if (coleccion instanceof Stack) {
+			return ((Stack<Color>) coleccion).get(posicion);
+		}
+		
+		return ((ArrayList<Color>) coleccion).get(posicion);
 	}
 
 }
--- src/test/ComprobadorTest.java
+++ src/test/ComprobadorTest.java
@@ -14,16 +14,6 @@ import control.Comprobador;
 class ComprobadorTest {
 	
 	Comprobador instancia = Comprobador.getInstance();
-
-	@Test
-	void testComprobarColoresIguales() {
-
-		Color[] colores = {Color.BLACK, Color.BLUE};
-		
-		assertTrue(instancia.comprobarColoresIguales(colores[0], colores[0]));
-		assertFalse(instancia.comprobarColoresIguales(colores[0], colores[1]));
-		
-	}
 	
 	@Test
 	void testIsColeccionLlena() {
@@ -32,7 +22,7 @@ class ComprobadorTest {
 		final int limiteBajo = 2;
 		final int limiteIgual = 5;
 		
-		ArrayList<Object> colecciones = new ArrayList<>();
+		ArrayList<AbstractCollection<Color>> colecciones = new ArrayList<>();
 		
 		colecciones.add(new ArrayDeque<Color>());
 		colecciones.add(new Stack<Color>());
@@ -40,16 +30,38 @@ class ComprobadorTest {
 		
 		for (int i = 0; i < colecciones.size(); i++) {
 			for (int j = 0; j < 5; j++) {
-				((AbstractCollection) colecciones.get(i)).add(Color.BLACK);
+				((AbstractCollection<Color>) colecciones.get(i)).add(Color.BLACK);
 			}
 		}
 		
 		for (int i = 0; i < 3; i++) {
+			System.out.println("PROBANDO SI COLECCION " + i);
+			
+			System.out.println("	* esta llena");
 			assertTrue(this.instancia.isColeccionLlena(colecciones.get(i), limiteBajo));
+			System.out.println("	* esta al limite");
 			assertTrue(this.instancia.isColeccionLlena(colecciones.get(i), limiteIgual));
+			System.out.println("	* esta sobrante");
 			assertFalse(this.instancia.isColeccionLlena(colecciones.get(i), limiteAlto));
 		}
 
+	}
+	
+	@Test
+	void testIsColoresIgualesContiguos() {
+		ArrayList<Color> coloresContiguos = new ArrayList<>();
+		ArrayList<Color> coloresNoContiguos = new ArrayList<>();
+		
+		for (int i = 0; i < 2; i++) {
+			coloresContiguos.add(Color.BLACK);
+			coloresContiguos.add(Color.red);
+			coloresContiguos.add(Color.BLACK);
+			coloresNoContiguos.add(Color.BLACK);
+			coloresNoContiguos.add(Color.RED);
+		}
+		
+		assertTrue(this.instancia.isColoresIgualesContiguos(coloresContiguos));
+		assertFalse(this.instancia.isColoresIgualesContiguos(coloresNoContiguos));
 	}
 
 }
