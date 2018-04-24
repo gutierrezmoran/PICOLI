6658ad6035c8643b040b715b5a5e5a93ce428d44
sergiogalan99 <sergiocalamonte2018@gmail.com>
2018-04-24 08:45:48 +0200

    HistorialTest

--- src/test/HistorialTest.java
+++ src/test/HistorialTest.java
@@ -6,23 +6,41 @@ import org.junit.jupiter.api.Test;
 
 import modelo.Colores;
 import modelo.Historial;
+import utiles.Constantes;
 
 class HistorialTest {
 
 	Historial historial = new Historial();
+
 	@Test
 	void testBuscarCoincidencias() {
-		
-		ArrayList<Colores> conCoincidencia=new ArrayList<>();
-		ArrayList<Colores> sinCoincidencia=new ArrayList<>();
-		
+
+		ArrayList<Colores> conCoincidencia = new ArrayList<>();
+		ArrayList<Colores> sinCoincidencia = new ArrayList<>();
+
 		conCoincidencia.add(Colores.amarillo);
 		conCoincidencia.add(Colores.amarillo);
 		sinCoincidencia.add(Colores.amarillo);
 		sinCoincidencia.add(Colores.rojo);
-		
+
 		assertTrue(this.historial.buscarCoincidencias(Colores.amarillo, conCoincidencia));
 		assertFalse(this.historial.buscarCoincidencias(Colores.amarillo, sinCoincidencia));
+	}
+
+	@Test
+	void testSustituirColor() {
+
+		final int HISTORICO_CASI_LLENO = Constantes.TAMANO_HISTORICO - 1;
+
+		for (int i = 0; i < HISTORICO_CASI_LLENO; i++) {
+			historial.getListaColores().add(Colores.amarillo);
+		}
+
+		historial.sustituirColor(Colores.azul);
+		assert (historial.getListaColores().get(historial.getListaColores().size()-1).equals(Colores.azul));
+		historial.sustituirColor(Colores.verde);
+		assert (historial.getListaColores().get(0).equals(Colores.amarillo)
+				&& historial.getListaColores().get(historial.getListaColores().size()-1).equals(Colores.verde));
 	}
 
 }
